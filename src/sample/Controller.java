5932cc7182d62aff400e5b71a21f6b2f7d3c78ed
tcbutler320 <tcbutler320@gmail.com>
2020-02-20 20:40:13 -0500

    link ls button to function

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,9 +1,17 @@
 package sample;
-import io.tylerbutler.*;
+
+import io.tylerbutler.tools.TerminalNavigation;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
 
 public class Controller {
 
-
-
+    @FXML
+    void clickLS(MouseEvent event) {
+        TerminalNavigation tn = new TerminalNavigation();
+        tn.getLs();
+    }
 }
