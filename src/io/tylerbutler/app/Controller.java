6669f81c44900db09158fef41c6ff342f0f59a22
tcbutler320 <tcbutler320@gmail.com>
2020-02-22 18:12:18 -0500

    add target settings support

--- src/io/tylerbutler/app/Controller.java
+++ src/io/tylerbutler/app/Controller.java
@@ -1,10 +1,12 @@
 package io.tylerbutler.app;
 
+import io.tylerbutler.test.TargetSettingsTest;
 import io.tylerbutler.tools.TerminalNavigation;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
@@ -18,14 +20,23 @@ public class Controller {
     }
 
     @FXML
-    private TextField display;
+    private Button setTargetBtn;
 
+    @FXML
+    private TextArea cmdQueTArea;
+
+    @FXML
+    void setTarget(MouseEvent event) {
+        cmdQueTArea.setText(TargetSettingsTest.targetSettingsTest());
+    }
+
+    // TODO remove display method with working method
     @FXML
     void clickLS(MouseEvent event) {
         TerminalNavigation tn = new TerminalNavigation();
         String test = tn.getLs();
         Platform.runLater(() -> {
-            display.setText(display.getText() + "log n°2 ...");
+//            display.setText(display.getText() + "log n°2 ...");
         });
     }
 }
---
+++ src/io/tylerbutler/test/TargetSettingsTest.java
@@ -0,0 +1,10 @@
+package io.tylerbutler.test;
+
+public class TargetSettingsTest {
+
+    public static String targetSettingsTest() {
+        String out = "Target has been changed";
+        return out;
+
+    }
+}
